d437d66dd6248bb57d1f7c9d7647a6a41517d545
Yi-YongMin <lym8467@naver.com>
2024-06-24 01:33:51 +0900

    6/24

--- baekjoon/bj2839.py
+++ baekjoon/bj2839.py
@@ -1,7 +1,15 @@
 N = int(input())
-cnt=-1
-a5,a3=0,0
-
-while(1):
-    N//5=a5
-    
+ans = N
+cnt = -1
+a5, a3 = 0, N // 3
+while 1:
+    a3 = N // 3
+    if a3 < 0:
+        break
+    if (a5 * 5 + a3 * 3) == ans:
+        cnt = a5 + a3
+        print("후보군 a5 : ", a5, " a3 : ", a3)
+    # print("a5 : ", a5, " a3 : ", a3, " N : ", N)
+    a5 += 1
+    N -= 5
+print(cnt)
