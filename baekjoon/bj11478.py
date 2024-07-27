6daefff21f92e61b7b5322297491a5e3dc42689c
Yi-YongMin <lym8467@naver.com>
2024-07-27 23:54:17 +0900

    Update bj11478.py

--- baekjoon/bj11478.py
+++ baekjoon/bj11478.py
@@ -1,7 +1,11 @@
-S = input()
-ans = set()
-tmp = len(S)
-for i in range(tmp + 1):
-    for j in range(tmp - i + 1):
-        ans.add(S[j : j + i])
-print(len(ans) - 1)
+# S = input()
+# ans = set()
+# tmp = len(S)
+# for i in range(tmp + 1):
+#     for j in range(tmp - i + 1):
+#         ans.add(S[j : j + i])
+# print(len(ans) - 1)
+asia = {"kr", "ch", "jp"}
+print(type(asia))
+asia.update({"ge", "kr", "hk"})
+print(asia)
