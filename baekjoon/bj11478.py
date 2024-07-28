ca1b761fec4c8a5ba8d4575ad17e622ebcb2c98e
Yi-YongMin <lym8467@naver.com>
2024-07-28 17:11:39 +0900

    data processing engineer success

--- baekjoon/bj11478.py
+++ baekjoon/bj11478.py
@@ -1,11 +1,8 @@
-# S = input()
-# ans = set()
-# tmp = len(S)
-# for i in range(tmp + 1):
-#     for j in range(tmp - i + 1):
-#         ans.add(S[j : j + i])
-# print(len(ans) - 1)
-asia = {"kr", "ch", "jp"}
-print(type(asia))
-asia.update({"ge", "kr", "hk"})
-print(asia)
+S = input()
+ans = set()
+tmp = len(S)
+for i in range(tmp + 1):
+    for j in range(tmp - i + 1):
+        ans.add(S[j : j + i])
+print(len(ans) - 1)
+
