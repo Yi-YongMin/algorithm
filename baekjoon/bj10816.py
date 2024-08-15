53936e7e7517a4159047d0e06f2c93845ebcdcd8
Yi-YongMin <lym8467@naver.com>
2024-08-16 00:10:17 +0900

    부산 2일차 - 알고리즘 1개

--- baekjoon/bj10816.py
+++ baekjoon/bj10816.py
@@ -1,6 +1,15 @@
 N = int(input())
 arr1 = list(map(int, input().split()))
 M = int(input())
-s2 = set()
-for i in range(M):
-    s
+arr2 = list(map(int, input().split()))
+s2 = {}
+for i in arr1:
+    if i in s2:
+        s2[i] += 1
+    else:
+        s2[i] = 1
+for i in arr2:
+    if i in s2:
+        print(s2[i], end=" ")
+    else:
+        print(0, end=" ")
